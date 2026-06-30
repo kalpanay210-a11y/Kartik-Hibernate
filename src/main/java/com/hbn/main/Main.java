6c51fd9a2211f60fe307110456ff1c02b9d8f98c
kalpanay210-a11y <kalpanay210@gmail.com>
2026-06-30 15:38:56 +0530

    Configuration

--- src/main/java/com/hbn/main/Main.java
+++ src/main/java/com/hbn/main/Main.java
@@ -4,32 +4,47 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import com.hbn.configuration.HibernateConfig;
 import com.hbn.entity.Employee;
 
 public class Main {
     public static void main(String[] args) {
         
-        // 1. Configuration aur SessionFactory initialize karein
-        Configuration cfg = new Configuration();
-        cfg.configure("hibernate.cfg.xml");
-        SessionFactory factory = cfg.buildSessionFactory();
-        
-        // 2. Naya Employee object banayein
-        Employee emp = new Employee("Aman", "Male", 25000, "GYZ");
-        
-        // 3. Session open karein aur Transaction shuru karein
-        Session session = factory.openSession();
-        Transaction tx = session.beginTransaction();
-        
-        // 4. Object ko database mein save karein
-        session.persist(emp); // Note: Agar purana hibernate ho toh session.save(emp); likhein
-        
-        // 5. Transaction commit karein (Yahan tak code sahi chala toh data permanent save ho jayega)
-        tx.commit();
-        System.out.println("Data successfully saved!");
-        
-        // 6. Resources close karein
-        session.close();
-        factory.close();
+//        // 1. Configuration aur SessionFactory initialize karein
+//        Configuration cfg = new Configuration();
+//        cfg.configure("hibernate.cfg.xml");
+//        SessionFactory factory = cfg.buildSessionFactory();
+//        
+//        // 2. Naya Employee object banayein
+//        Employee emp = new Employee("Aman", "Male", 25000, "GYZ");
+//        
+//        // 3. Session open karein aur Transaction shuru karein
+//        Session session = factory.openSession();
+//        Transaction tx = session.beginTransaction();
+//        
+//        // 4. Object ko database mein save karein
+//        session.persist(emp); // Note: Agar purana hibernate ho toh session.save(emp); likhein
+//        
+//        // 5. Transaction commit karein (Yahan tak code sahi chala toh data permanent save ho jayega)
+//        tx.commit();
+//        System.out.println("Data successfully saved!");
+//        
+//        // 6. Resources close karein
+//        session.close();
+//        factory.close();
+    	
+	   Employee e = new Employee( "Vikas", "male", 330000, "HYD");
+
+		
+		Session session = HibernateConfig.getSessionFactory().openSession();
+//		4.
+		Transaction tx = session.beginTransaction();
+		
+		session.persist(e);
+		
+		tx.commit();
+		
+		
     }
 }
